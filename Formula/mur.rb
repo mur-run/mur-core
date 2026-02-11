0f2f8a0f24dd1a685b221c76846d9ab76ab1c43e
david <karajanchang@gmail.com>
2026-02-11 10:57:43 +0800

    chore: update Homebrew formula for v1.4.1

--- Formula/mur.rb
+++ Formula/mur.rb
@@ -1,8 +1,8 @@
 class Mur < Formula
   desc "Invisible continuous learning system for AI coding assistants"
   homepage "https://github.com/mur-run/mur-core"
-  url "https://github.com/mur-run/mur-core/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "PLACEHOLDER"  # Will be updated by release workflow
+  url "https://github.com/mur-run/mur-core/archive/refs/tags/v1.4.1.tar.gz"
+  sha256 "da9db9d1f0493f2c3acd64efe50978e886bc259ee98d0be01faf1b0fb4dbe84a"
   license "MIT"
   head "https://github.com/mur-run/mur-core.git", branch: "main"
 
@@ -10,14 +10,20 @@ class Mur < Formula
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/mur"
+    ldflags = %W[
+      -s -w
+      -X github.com/mur-run/mur-core/cmd/mur/cmd.Version=#{version}
+      -X github.com/mur-run/mur-core/cmd/mur/cmd.Commit=#{tap.user}
+      -X github.com/mur-run/mur-core/cmd/mur/cmd.BuildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "./cmd/mur"
   end
 
   def post_install
-    ohai "mur installed! Run 'mur init' to get started."
+    ohai "mur installed! Run 'mur init --hooks' to get started."
   end
 
   test do
-    assert_match "mur #{version}", shell_output("#{bin}/mur version")
+    assert_match version.to_s, shell_output("#{bin}/mur version")
   end
 end
